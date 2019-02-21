7a5e05e7cc213f540efda266df59566e3e7feb53
PENG,CHAO-WEI <alanwei1202@gmail.com>
2019-02-21 14:53:11 +0800

    [add] finish stored procedure HW

---
+++ WebSP/WebForm1.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebSP.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <table>
+                <tr>
+                    <td colspan="4">
+                        <b>Search Person</b>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <b>Name</b>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtNameLike" runat="server"></asp:TextBox>
+                    </td>
+                    <td>
+                        <b>Salary > </b>
+                    </td>
+                    <td>
+                        <asp:TextBox ID="txtSalaryGreaterThan" runat="server"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="4">
+                        <asp:Button ID="btnSerach" runat="server" Text="Search"
+                            OnClick="btnSerach_Click" />
+                    </td>
+                </tr>
+                <tr>
+                    <td colspan="4">
+                        <asp:GridView ID="gvEmployees" runat="server">
+                        </asp:GridView>
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebSP/WebForm1.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebSP
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+                GetData();
+        }
+        protected void btnSerach_Click(object sender, EventArgs e)
+        {
+            GetData();
+        }
+        private void AttachParameter(SqlCommand command, string parameterName, Control control)
+        {
+            if (control is TextBox && ((TextBox)control).Text != string.Empty)
+            {
+                var parameter = new SqlParameter(parameterName, ((TextBox)control).Text);
+                command.Parameters.Add(parameter);
+            }
+            else if (control is DropDownList && ((DropDownList)control).SelectedValue != "-1")
+            {
+                var parameter = new SqlParameter(parameterName, ((DropDownList)control).SelectedValue);
+                command.Parameters.Add(parameter);
+            }
+        }
+        private void GetData()
+        {
+            //string cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            string cs = ConfigurationManager.ConnectionStrings["SampleConnectionString"].ConnectionString;
+            using (var con = new SqlConnection(cs))
+            {
+                var cmd = new SqlCommand("spSearchPerson3", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                AttachParameter(cmd, "@NameLike", txtNameLike);
+                AttachParameter(cmd, "@SalaryGreaterThan", txtSalaryGreaterThan);
+                con.Open();
+                gvEmployees.DataSource = cmd.ExecuteReader();
+                gvEmployees.DataBind();
+            }
+        }
+    }
+}
